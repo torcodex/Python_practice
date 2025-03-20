716270fa4a78aeebb895fd256ef5a6bf686c90f3
Antony Thomas <134713648+torcodex@users.noreply.github.com>
2025-03-21 01:24:30 +0530

    Create LuckyNumber.py

---
+++ LuckyNumber.py
@@ -0,0 +1,9 @@
+a = int(input())
+if  a%6==0:
+    print("Number is divisible by 6")
+elif a%3==0:
+    print("Number is divisible by 3")
+elif a%2==0:
+    print("Number is divisible by 2")
+else:
+    print("Number is not divisible by 2, 3 or 6")
