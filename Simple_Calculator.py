24859bd59777395ffc42222915bb2146ec8242fc
Antony Thomas <134713648+torcodex@users.noreply.github.com>
2025-03-21 20:18:56 +0530

    Create Simple_Calculator.py

---
+++ Simple_Calculator.py
@@ -0,0 +1,13 @@
+a = input()
+b = int(input())
+c = int(input())
+if a=="+":
+    print(b+c)
+elif a=="-":
+    print(b-c)
+elif a=="*":
+    print(b*c)
+elif a=="/":
+    print(b/c)
+else:
+    print(b%c)
