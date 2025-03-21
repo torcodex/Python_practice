2a41e109fd2c405205e6ef7553c4614b3ecb8e0c
Antony Thomas <134713648+torcodex@users.noreply.github.com>
2025-03-21 20:15:49 +0530

    Create season.py

---
+++ season.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a == 2 or a == 3:
+    print("Spring")
+elif a == 4 or a == 5 or a == 6:
+    print("Summer")
+elif a == 8 or a == 7:
+    print("Rainy")
+elif a == 10 or a == 9:
+    print("Autumn")
+elif a == 12 or a == 11 or a == 1:
+    print("Winter")
